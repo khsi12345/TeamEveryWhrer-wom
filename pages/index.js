92807d9ee1abd12d5cc9be351f51d3efdc66cc44
khsi12345 <khsi6237@gmail.com>
2019-10-19 21:50:43 +0900

    first commit

---
+++ pages/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Head from 'next/head'
+import Nav from '../components/nav'
+
+import '../style/scss/index.scss';
+
+const Home = () => (
+  <div>
+    <Head>
+      <h1 className="home">Home</h1>
+      <link rel='icon' href='/favicon.ico' />
+    </Head>
+
+    {/* <Nav /> */}
+  </div>
+);
+export default Home

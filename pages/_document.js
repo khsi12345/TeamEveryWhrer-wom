748ec5ef00d70423f5a8212f464feab214e390fb
Doori Kim <doori.alice.kim@gmail.com>
2019-10-28 11:22:40 +0900

    added _document.js and new build

---
+++ pages/_document.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import Document, { Head, Main, NextScript } from 'next/document';
+import flush from 'styled-jsx/server';
+
+export default class MyDocument extends Document {
+  static getInitialProps({ renderPage }) {
+    const { html, head, errorHtml, chunks } = renderPage();
+    const styles = flush();
+    return { html, head, errorHtml, chunks, styles };
+  }
+
+  render() {
+    return (
+      <html lang="en">
+        <Head>
+          <meta charSet="utf-8" />
+
+          {/* <title>My page</title> */}
+          <link rel="stylesheet" href="resetstyle.js" />
+        </Head>
+        <body className="custom_class">
+          {this.props.customValue}
+          <Main />
+          <NextScript />
+        </body>
+      </html>
+    );
+  }
+}
